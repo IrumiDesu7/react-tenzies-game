b3b07812c1f5cf17fb74f65d81e37f07ccbb4b47
IrumiDesu7 <ikalam89@gmail.com>
2022-06-28 14:41:50 +0800

    add button to roll dices

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import Die from './components/Die';
 
 function App() {
+  function allNewDice() {
+    const randArray = [];
+    for (let i = 0; i < 10; i++) {
+      randArray.push(Math.ceil(Math.random() * 6));
+    }
+    return randArray;
+  }
+
+  const [dices, setDices] = useState(allNewDice());
+
+  const diceElement = dices.map((item, index) => (
+    <Die key={index} value={item} />
+  ));
+
+  function rollDice() {
+    setDices(allNewDice());
+  }
+
   return (
     <main>
       <div className='game-container'>
-        <div className='die-container'>
-          <Die value={3} />
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={5} />
-          <Die value={5} />
-          <Die value={6} />
-          <Die value={4} />
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={6} />
+        <div className='die-container'>{diceElement}</div>
+        <div className='roll-btn' onClick={rollDice}>
+          Roll Dice
         </div>
       </div>
     </main>
